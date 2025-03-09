5c713fdbcac06946a2d4f1b2f293305832627b83
Seu Nome <mandoqxo@gmail.com>
2025-03-08 23:50:30 -0300

    exemplo de operador ternário em Java

---
+++ fundamentos/src/Operadores/Ternario.java
@@ -0,0 +1,11 @@
+package operadores;
+
+public class Ternario {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+       double media = 7.6;
+         String resultado = media >= 5.0 ? "em recuperacao" : "aprovado";
+
+         System.out.println("O aluno esta  " + resultado);
+    }
+}
