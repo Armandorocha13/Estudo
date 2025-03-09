a9db02aa472ea97902a494fe14bb2ea4713ccc20
Seu Nome <mandoqxo@gmail.com>
2025-03-08 23:47:48 -0300

    Exemplo de operadores unários em Java

---
+++ fundamentos/src/Operadores/Unarios.java
@@ -0,0 +1,17 @@
+package operadores;
+
+public class Unarios {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int a = 1;
+        int b = 2;
+        
+        // Operador de incremento
+        System.out.println(a++); // 1
+        System.out.println(a); // 2
+        
+        // Operador de decremento
+        System.out.println(--b); // 1
+        System.out.println(b); // 1
+    }
+}
